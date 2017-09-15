617118aeda33f0962103e9a9860e8509f12c2128
Tang Rufus <tangrufus@gmail.com>
2017-09-16 00:39:07 +0800

    Support Ansible vault encrypted config file

--- lib/vagrant_plugins/trellis_sequel/commands/open.rb
+++ lib/vagrant_plugins/trellis_sequel/commands/open.rb
@@ -4,6 +4,7 @@ require "erb"
 require "ostruct"
 require "optparse"
 require "vagrant"
+require "vagrant_plugins/trellis_sequel/vault"
 
 module VagrantPlugins
   module TrellisSequel
@@ -15,12 +16,16 @@ module VagrantPlugins
             o.banner = "Usage: vagrant trellis-sequel open [options] [vm-id]"
             o.separator ""
 
-            o.on("-u", "--user DB_USER", String, "Required. Database username.") do |user|
-              options[:user] = user
+            o.on("--site [site]", String, "Site whose database going to be opened.") do |site|
+              options[:site] = site
             end
 
-            o.on("-p", "--password DB_PASSWORD", String, "Required. Value of db_password from group_vars/development/vault.yml") do |password|
-              options[:password] = password || "example_dbpassword"
+            o.on("--vault-password-file [VAULT_PASSWORD_FILE]", String, "Vault password file.") do |vault_password_file|
+              options[:vault_password_file] = vault_password_file
+            end
+
+            o.on("--vault-pass [VAULT_PASS]", String, "Vault password.") do |vault_password|
+              options[:vault_password] = vault_password
             end
 
             o.on("-h", "--help", "Print this help") do
@@ -30,51 +35,55 @@ module VagrantPlugins
           end
           argv = parse_options(opts)
 
-          # Check required options
-          missing_required_options = [:user, :password].any? { |param| options[param].nil? }
-          fail Vagrant::Errors::CLIInvalidOptions.new(help: "Missing required options.") if missing_required_options
-
-          @env.ui.info("Generating SPF file...")
-
-          # Load the SPF template
-          template_path = File.join(File.dirname(__FILE__), "/..", "template.spf")
-          template = ERB.new(File.read(template_path), nil, "%<>")
-
           with_target_vms(argv) do |machine|
-           # Collect data
-           ssh_info = machine.ssh_info
-           raise Vagrant::Errors::SSHNotReady if ssh_info.nil?
+            # Collect SSL data
+            ssh_info = machine.ssh_info
+            raise Vagrant::Errors::SSHNotReady if ssh_info.nil?
 
-           data = {
-             "database" => {
-               "name" => "#{options[:user]}_development",
-              "user" => options[:user],
-              "password" => options[:password],
-            },
-            "ssh" => {
-              "host" => ssh_info[:host],
-              "port" => ssh_info[:port],
-              "user" => ssh_info[:username],
-              "private_key_path" => ssh_info[:private_key_path],
+            options[:vault_password] ||= vault_password_from(path: options[:vault_password_file], machine: machine)
+
+            # Collect database data
+            vault_path = File.join(machine.env.root_path, "group_vars/development/vault.yml")
+            vault = Vault.new(path: vault_path, password: options[:vault_password])
+
+            data = {
+                database: vault.database_for(site: options[:site]),
+                ssh: {
+                    "host": ssh_info[:host],
+                    "port": ssh_info[:port],
+                    "user": ssh_info[:username],
+                    "private_key_path": ssh_info[:private_key_path],
+                }
             }
-          }
 
-           # Output template
-           content = template.result OpenStruct.new(data).instance_eval { binding }
+            # Output template
+            content = template.result OpenStruct.new(data).instance_eval { binding }
 
-           # Write it to the file
-           path = File.join(@env.tmp_path, "#{data["database"]["name"]}.spf")
-           file = File.open(path, "w")
-           file.write(content)
-           file.close()
+            # Write it to the file
+            path = File.join(@env.tmp_path, "#{data[:database][:name]}.spf")
+            file = File.open(path, "w")
+            file.write(content)
+            file.close
 
-           # And, open it
-           system("open \"#{path}\"")
-         end
+            # And, open it
+            system("open \"#{path}\"")
+          end
 
           # Always exit with success
           0
-       end
+        end
+
+        private
+
+          def vault_password_from(path: nil, machine: nil)
+            path ||= File.join(machine.env.root_path, ".vault_pass")
+            File.read(path).chomp
+          end
+
+          def template
+            path = File.join(File.dirname(__FILE__), "/..", "template.spf")
+            ERB.new(File.read(path), nil, "%<>")
+          end
       end
     end
   end
